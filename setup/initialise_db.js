7ec2533ff52ba3d13a70e08cdb792e8bea26493a
Koh Jun Dong <KohComputer@gmail.com>
2022-09-11 21:43:34 +0800

    Other cleanup

--- setup/initialise_db.js
+++ setup/initialise_db.js
@@ -1,4 +1,4 @@
-conn = new Mongo();
+let conn = new Mongo();
 db = conn.getDB("admin");
 
 print("Creating root_admin as root");
@@ -23,6 +23,7 @@ db.createUser({
 
 print("\nCreating DB gsod, a few collections and various indices.");
 db = db.getSiblingDB("gsod");
+db.createCollection("stations");
 db.createCollection("weatherData", {
   timeseries: {
     timeField: "timestamp",
@@ -30,6 +31,13 @@ db.createCollection("weatherData", {
     granularity: "hours"
   }
 });
+
+db.stations.createIndex({
+  "station.location": "2dsphere",
+  "station.name": 1,
+  "station.country.name": 1,
+  "station.elevation": 1,
+}, { collation: { locale: "en", strength: 1 } });
 
 // Create index for station location, name, country name and elevation
 db.weatherData.createIndex({
