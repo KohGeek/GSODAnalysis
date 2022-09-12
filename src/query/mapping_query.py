6bf722188279344ff5f5d6d9f28f64b27c00dab3
Koh Jun Dong <KohComputer@gmail.com>
2022-09-13 00:51:09 +0800

    Numerous changes * Cleanup unused imports * Rename variables to be more verbose * Temps are now in Celcius * Legend is now labelled

--- src/query/mapping_query.py
+++ src/query/mapping_query.py
@@ -3,52 +3,53 @@ from datetime import datetime, timedelta
 import cartopy.crs as ccrs
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import numpy as np
 
 from gsodtools.getdb import get_db
 
 
 def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+    for i in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(i)
 
 def main():
     client = get_db(1)
-    db = client["gsod"]
+    database = client["gsod"]
 
     plt.ion()
 
     cmap = plt.get_cmap('rainbow')
-    norm = mpl.colors.Normalize(vmin=-20, vmax=100)
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-    sm.set_array([])
-    
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-    fig.colorbar(sm)
-    ax.coastlines()
+    norm = mpl.colors.Normalize(vmin=-25, vmax=40)
+    smap = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    smap.set_array([])
 
-    stations = list(db.stations.aggregate([
+    fig = plt.figure(figsize=(10, 10))
+    axis = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    axis.coastlines()
+
+    cbar = fig.colorbar(smap)
+    cbar.ax.set_ylabel('Temperature (C)', rotation=270, fontsize=15, labelpad=20)
+
+    stations = list(database.stations.aggregate([
         {"$sample": {"size": 1000}},
         {"$project": {"_id": 1}}
     ]))
 
     stations = [s["_id"] for s in stations]
 
-    start_date = datetime(2019, 1, 1)
-    end_date = datetime(2019, 12, 31)
+    start_date = datetime(2016, 1, 1)
+    end_date = datetime(2021, 12, 31)
     for single_date in daterange(start_date, end_date):
 
-        data = list(db.weatherData.aggregate([
+        data = list(database.weatherData.aggregate([
             {"$match": {"$and": [{"timestamp": single_date}, {"station._id": {"$in": stations}}]}},
             {"$project": {"_id": 0, "location": "$station.location.coordinates", "temperature": 1}},
             {"$sample": {"size": 1000}},
         ]))
 
-        scat = ax.scatter([x["location"][0] for x in data],
+        scat = axis.scatter([x["location"][0] for x in data],
             [x["location"][1] for x in data],
-            c=[x["temperature"] for x in data],
-            s=20, cmap="rainbow", vmax=100, vmin=-20,
+            c=[(x["temperature"] - 32) * 5 / 9 for x in data],
+            s=20, cmap="rainbow", vmax=40, vmin=-25,
             transform=ccrs.PlateCarree())
 
         # draw title
