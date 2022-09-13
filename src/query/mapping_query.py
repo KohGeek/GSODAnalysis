262cec83ef2be00f7bc6c62c0c77e709a7354f5e
Koh Jun Dong <KohComputer@gmail.com>
2022-09-13 12:00:17 +0800

    Refactor map query

--- src/query/mapping_query.py
+++ src/query/mapping_query.py
@@ -11,6 +11,7 @@ def daterange(start_date, end_date):
     for i in range(int((end_date - start_date).days)):
         yield start_date + timedelta(i)
 
+
 def main():
     client = get_db(1)
     database = client["gsod"]
@@ -18,7 +19,9 @@ def main():
     plt.ion()
 
     cmap = plt.get_cmap('rainbow')
-    norm = mpl.colors.Normalize(vmin=-25, vmax=40)
+    vmax = 40
+    vmin = -25
+    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
     smap = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     smap.set_array([])
 
@@ -27,7 +30,8 @@ def main():
     axis.coastlines()
 
     cbar = fig.colorbar(smap)
-    cbar.ax.set_ylabel('Temperature (C)', rotation=270, fontsize=15, labelpad=20)
+    cbar.ax.set_ylabel('Temperature (C)', rotation=270,
+                       fontsize=15, labelpad=20)
 
     stations = list(database.stations.aggregate([
         {"$sample": {"size": 1000}},
@@ -41,16 +45,17 @@ def main():
     for single_date in daterange(start_date, end_date):
 
         data = list(database.weatherData.aggregate([
-            {"$match": {"$and": [{"timestamp": single_date}, {"station._id": {"$in": stations}}]}},
-            {"$project": {"_id": 0, "location": "$station.location.coordinates", "temperature": 1}},
-            {"$sample": {"size": 1000}},
+            {"$match": {"$and": [{"timestamp": single_date}, {
+                "station._id": {"$in": stations}}]}},
+            {"$project": {
+                "_id": 0, "location": "$station.location.coordinates", "temperature": 1}},
         ]))
 
         scat = axis.scatter([x["location"][0] for x in data],
-            [x["location"][1] for x in data],
-            c=[(x["temperature"] - 32) * 5 / 9 for x in data],
-            s=20, cmap="rainbow", vmax=40, vmin=-25,
-            transform=ccrs.PlateCarree())
+                            [x["location"][1] for x in data],
+                            c=[(x["temperature"] - 32) * 5 / 9 for x in data],
+                            s=30, cmap=cmap, vmax=vmax, vmin=vmin,
+                            transform=ccrs.PlateCarree())
 
         # draw title
         plt.title(single_date.strftime('%d/%m/%Y'), fontsize=30)
